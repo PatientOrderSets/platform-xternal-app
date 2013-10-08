18827148de92b39e58de61fc7c82f1c2cbabe7da
Alfred Wong <alfred.wong@patientordersets.com>
2013-10-07 23:12:03 -0400

    Added new columns to the user

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007213813) do
+ActiveRecord::Schema.define(version: 20131007220331) do
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "first_name",         default: "", null: false
-    t.string   "last_name",          default: "", null: false
-    t.string   "upn",                default: "", null: false
-    t.integer  "sign_in_count",      default: 0,  null: false
+    t.string   "email",                    default: "", null: false
+    t.string   "first_name",               default: "", null: false
+    t.string   "last_name",                default: "", null: false
+    t.string   "upn",                      default: "", null: false
+    t.integer  "sign_in_count",            default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "doorkeeper_access_token"
+    t.string   "doorkeeper_refresh_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
